dc4510399a457d57a6dc6137ad821914cd31780a
Yuki Iwabuchi <8ucchiman@YukisMacBook-Air.local>
2023-05-16 23:03:46 +0900

    hologram added

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -216,7 +216,14 @@ require("lazy").setup(
                 }
             })
         end,
-
+    },
+    {
+        "edluffy/hologram.nvim",
+        config = function()
+            require('hologram').setup{
+                auto_display = true -- WIP automatic markdown image display, may be prone to breaking
+            }
+        end
     }
 
     --{
