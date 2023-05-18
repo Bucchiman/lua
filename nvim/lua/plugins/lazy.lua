e398af81c3736055559ed01973ed1b783222b3e8
8ucchiman <yukibuchi17@keio.jp>
2023-05-18 18:26:16 +0900

    snippets added

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -258,6 +258,22 @@ require("lazy").setup(
                 },
             })
         end
+    },
+    {
+        "dcampos/nvim-snippy",
+        config = function()
+            require('snippy').setup({
+                mappings = {
+                    is = {
+                        ['<Tab>'] = 'expand_or_advance',
+                        ['<S-Tab>'] = 'previous'
+                    },
+                    nx = {
+                        ['<leader>x'] = 'cut_text',
+                    },
+                },
+            })
+        end
     }
 
     --{
