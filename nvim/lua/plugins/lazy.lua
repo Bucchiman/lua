ce333ecf4efd8ce1e56ff0c1ccdfd11f001bacfc
8ucchiman <yukibuchi17@keio.jp>
2023-05-23 18:59:33 +0900

    snippets fixed

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -146,6 +146,7 @@ require("lazy").setup(
         }
     },
     {
+        -- OK(05/22)
         "neovim/nvim-lspconfig",
         lazy = false,
         dependencies = {
@@ -155,6 +156,7 @@ require("lazy").setup(
         },
     },
     {
+        -- OK(05/22)
         "williamboman/mason.nvim",
         dependencies = {
           "williamboman/mason-lspconfig.nvim",
@@ -285,20 +287,48 @@ require("lazy").setup(
     --}
 })
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 local cmp = require("cmp")
+local snippy = require("snippy")
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
+  -- snippet = {
+  --   expand = function(args)
+  --     vim.fn["vsnip#anonymous"](args.body)
+  --   end,
+  -- },
+
   sources = {
     { name = "nvim_lsp" },
     -- { name = "buffer" },
     -- { name = "path" },
   },
   mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif snippy.can_expand_or_advance() then
+        snippy.expand_or_advance()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif snippy.can_jump(-1) then
+        snippy.previous()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ['<C-l>'] = cmp.mapping.complete(),
