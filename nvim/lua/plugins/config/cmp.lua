45d32a93c198f5dd61ce3a7a9367f945f172a222
Yuki Iwabuchi <yukibuchi17@keio.jp>
2023-07-23 23:52:50 +0900

    fixed keybind

--- nvim/lua/plugins/config/cmp.lua
+++ nvim/lua/plugins/config/cmp.lua
@@ -10,8 +10,6 @@
 --
 
 
-
-
 local cmp = require("cmp")
 
 cmp.setup({
@@ -27,33 +25,36 @@ cmp.setup({
         -- { name = "path" },
     },
     mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif snippy.can_expand_or_advance() then
-                snippy.expand_or_advance()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
-            end
-            end, { "i", "s" }),
-            ["<S-Tab>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    endendcmp.select_prev_item()
-                elseif snippy.can_jump(-1) then
-                    snippy.previous()
-                else
-                    fallback()
-                end
-            end, { "i", "s" }),
-            ["<C-p>"] = cmp.mapping.select_prev_item(),
-            ["<C-n>"] = cmp.mapping.select_next_item(),
-            ['<C-l>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ["<CR>"] = cmp.mapping.confirm { select = true },
-        }),
-        experimental = {
-            ghost_text = true,
-        },
-    })
+        -- ["<Tab>"] = cmp.mapping(function(fallback)
+        --     if cmp.visible() then
+        --         cmp.select_next_item()
+        --     elseif snippy.can_expand_or_advance() then
+        --         snippy.expand_or_advance()
+        --     elseif has_words_before() then
+        --         cmp.complete()
+        --     else
+        --         fallback()
+        --     end
+        --     end, { "i", "s" }),
+        --     ["<S-Tab>"] = cmp.mapping(function(fallback)
+        --         if cmp.visible() then
+        --             endendcmp.select_prev_item()
+        --         elseif snippy.can_jump(-1) then
+        --             snippy.previous()
+        --         else
+        --             fallback()
+        --         end
+        --     end, { "i", "s" }),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ['<C-l>'] = cmp.mapping.complete(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    experimental = {
+        ghost_text = true,
+    },
+})
