c8ac5040120d78e093079f11b94c7e0cbf1e849f
Yuki Iwabuchi <yukibuchi17@keio.jp>
2023-07-27 00:01:14 +0900

    transparency added

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -31,6 +31,21 @@ local venv = os.getenv("VIRTUAL_ENV")
 
 require("lazy").setup({
     {
+        "xiyaowong/transparent.nvim",
+        config = function ()
+            require("transparent").setup({
+              groups = { -- table: default groups
+                'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+                'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+                'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+                'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+              },
+              extra_groups = {}, -- table: additional groups that should be cleared
+              exclude_groups = {}, -- table: groups you don't want to clear
+            })
+        end
+    },
+    {
         "cocopon/iceberg.vim",
         config = function ()
             vim.cmd.colorscheme "iceberg"
