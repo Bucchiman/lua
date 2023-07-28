14d964f161732d6b6c669bae9375f11badb6dbcf
Yuki Iwabuchi <yukibuchi17@keio.jp>
2023-07-29 01:32:16 +0900

    plugin updated for image viewer

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -30,6 +30,26 @@ local img_previewer = vim.fn.executable("ueberzug") == 1 and { "ueberzug", "laye
 local venv = os.getenv("VIRTUAL_ENV")
 
 require("lazy").setup({
+    "nvim-lua/popup.nvim",
+    {
+        "nvim-telescope/telescope.nvim",
+        config = function ()
+            -- require('telescope').load_extension('media_files')
+            require'telescope'.setup {
+              extensions = {
+                media_files = {
+                  -- filetypes whitelist
+                  -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+                  filetypes = {"png", "webp", "jpg", "jpeg"},
+                  -- find command (defaults to `fd`)
+                  find_cmd = "rg"
+                }
+              },
+            }
+        end
+
+    },
+    "nvim-telescope/telescope-media-files.nvim",
     {
         "xiyaowong/transparent.nvim",
         config = function ()
