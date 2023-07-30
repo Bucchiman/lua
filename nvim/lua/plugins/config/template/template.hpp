3838984fde33d9194b3c3e882d59822359a4c93e
Yuki Iwabuchi <8ucchiman@YukisMacBook-Air.local>
2023-07-30 14:41:09 +0900

    refactoring

---
+++ nvim/lua/plugins/config/template/template.hpp
@@ -0,0 +1,9 @@
+/*
+ * FileName:     {{_file_name_}}
+ * Author:       {{_author_}}
+ * CreatedDate:  {{_date_}}
+ * LastModified: 2023-03-28 10:34:14 +0900
+ * Reference:    8ucchiman.jp
+ * Description:  ---
+ */
+
---
+++ nvim/lua/plugins/config/template/template.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# FileName:     {{_file_name_}}
+# Author:       {{_author_}}
+# CreatedDate:  {{_date_}}
+# LastModified: 2023-02-18 14:28:37 +0900
+# Reference:    8ucchiman.jp
+# Description:  ---
+#
+
+
+import os
+import sys
+# import utils
+# from utils import get_ml_args, get_dl_args, get_logger
+# import numpy as np
+# import pandas as pd
+
+
+def main():
+    # args = utils.get_args()
+    # method = getattr(utils, args.method)
+    {{_cursor_}}
+    pass
+
+
+if __name__ == "__main__":
+    main()
