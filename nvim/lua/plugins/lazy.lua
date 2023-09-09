c008a9ba085d6d862fb31926f9423a52d90f89a6
Yuki Iwabuchi <8ucchiman@YukisMacBook-Air.local>
2023-09-09 15:45:59 +0900

    added a plugin for git issue, pr

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -33,6 +33,17 @@ local venv = os.getenv("VIRTUAL_ENV")
 
 require("lazy").setup({
     {
+        'pwntester/octo.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = function ()
+            require('octo').setup()
+        end
+    },
+    {
         'willothy/flatten.nvim',
         config = true,
         lazy = false,
