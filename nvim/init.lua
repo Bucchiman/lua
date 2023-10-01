3543db5f8c0fb038dbe6367de3968de13bdd2dd9
8ucchiman <8ucchiman@gmail.com>
2023-10-02 00:37:17 +0900

    fix file exists

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,12 +24,19 @@ require("keymaps")
 require("plugins.lazy")
 require("tools.settings")
 local experiments = require("experiments")
-if vim.fn.filereadable(vim.fn.expand("$HOME/.config/local/local.lua")) then
+function file_exists(name)
+   local f=io.open(name, "r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
+-- if vim.fn.filereadable("$HOME/.config/local/lua/local.lua") then
+if file_exists("$HOME/.config/local/lua/local.lua") then
     vim.opt.runtimepath:append('$HOME/.config/local')
     require("local")
 end
 
-if vim.fn.filereadable(vim.fn.expand("/tmp/8ucchiman/nvim")) then
+-- if vim.fn.filereadable(vim.fn.expand("/tmp/8ucchiman/nvim")) then
+if file_exists("/tmp/8ucchiman/nvim/sample.lua") then
     vim.opt.runtimepath:append('/tmp/8ucchiman')
     require("sample")
 end
