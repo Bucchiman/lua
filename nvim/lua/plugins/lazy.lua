5bb985688fc8ff81bb87fcbf7117f003af78169b
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-10-15 12:08:55 +0900

    add some shortcut

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -65,6 +65,7 @@ require("lazy").setup({
     'jbyuki/nabla.nvim',
     {
         "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         build = "cd app && npm install",
         init = function()
             vim.g.mkdp_filetypes = { "markdown" } end,
