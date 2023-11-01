842c182fef1682090745fc88343cbc8812495196
8ucchiman <8ucchiman@gmail.com>
2023-11-01 11:47:33 +0900

    gist plugin

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -58,6 +58,19 @@ local venv = os.getenv("VIRTUAL_ENV")
 
 require("lazy").setup({
     {
+        "Rawnly/gist.nvim",
+        cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
+        config = true
+    },
+    -- `GistsList` opens the selected gif in a terminal buffer,
+    -- nvim-unception uses neovim remote rpc functionality to open the gist in an actual buffer
+    -- and prevents neovim buffer inception
+    {
+        "samjwill/nvim-unception",
+        lazy = false,
+        init = function() vim.g.unception_block_while_host_edits = true end
+    },
+    {
         'https://codeberg.org/esensar/nvim-dev-container',
         dependencies = 'nvim-treesitter/nvim-treesitter',
         config = function ()
