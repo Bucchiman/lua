d6c9f5f6b9f16e37353adde5372960ec3af6e04c
8ucchiman <8ucchiman@gmail.com>
2023-12-11 23:40:59 +0900

    fix for wsl

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -2,7 +2,7 @@
 -- Author:       8ucchiman
 -- Email:        8ucchiman@gmail.com
 -- CreatedDate:  2023-06-03 15:39:49
--- LastModified: 2023-01-23 14:18:33 +0900
+-- LastModified: 2023-12-11 23:07:35
 -- Reference:    https://github.com/MiaadTeam/lesvim/blob/main/lua/lazy/plugins.lua
 -- Description:  ---
 --
@@ -716,57 +716,57 @@ require("lazy").setup({
     {
         'equalsraf/neovim-gui-shim'
     },
-    {
-        'nosduco/remote-sshfs.nvim',
-        dependencies = {
-            {'nvim-telescope/telescope.nvim'}
-        },
-        config = function ()
-            require('remote-sshfs').setup{
-              connections = {
-                ssh_configs = { -- which ssh configs to parse for hosts list
-                  vim.fn.expand "$HOME" .. "/.ssh/config",
-                  "/etc/ssh/ssh_config",
-                  -- "/path/to/custom/ssh_config"
-                },
-                sshfs_args = { -- arguments to pass to the sshfs command
-                  "-o reconnect",
-                  "-o ConnectTimeout=5",
-                },
-              },
-              mounts = {
-                base_dir = vim.fn.expand "$HOME" .. "/.sshfs/", -- base directory for mount points
-                unmount_on_exit = true, -- run sshfs as foreground, will unmount on vim exit
-              },
-              handlers = {
-                on_connect = {
-                  change_dir = true, -- when connected change vim working directory to mount point
-                },
-                on_disconnect = {
-                  clean_mount_folders = false, -- remove mount point folder on disconnect/unmount
-                },
-                on_edit = {}, -- not yet implemented
-              },
-              ui = {
-                select_prompts = false, -- not yet implemented
-                confirm = {
-                  connect = true, -- prompt y/n when host is selected to connect to
-                  change_dir = false, -- prompt y/n to change working directory on connection (only applicable if handlers.on_connect.change_dir is enabled)
-                },
-              },
-              log = {
-                enable = false, -- enable logging
-                truncate = false, -- truncate logs
-                types = { -- enabled log types
-                  all = false,
-                  util = false,
-                  handler = false,
-                  sshfs = false,
-                },
-              },
-            }
-        end
-    },
+    -- {
+    --     'nosduco/remote-sshfs.nvim',
+    --     dependencies = {
+    --         {'nvim-telescope/telescope.nvim'}
+    --     },
+    --     config = function ()
+    --         require('remote-sshfs').setup{
+    --           connections = {
+    --             ssh_configs = { -- which ssh configs to parse for hosts list
+    --               vim.fn.expand "$HOME" .. "/.ssh/config",
+    --               "/etc/ssh/ssh_config",
+    --               -- "/path/to/custom/ssh_config"
+    --             },
+    --             sshfs_args = { -- arguments to pass to the sshfs command
+    --               "-o reconnect",
+    --               "-o ConnectTimeout=5",
+    --             },
+    --           },
+    --           mounts = {
+    --             base_dir = vim.fn.expand "$HOME" .. "/.sshfs/", -- base directory for mount points
+    --             unmount_on_exit = true, -- run sshfs as foreground, will unmount on vim exit
+    --           },
+    --           handlers = {
+    --             on_connect = {
+    --               change_dir = true, -- when connected change vim working directory to mount point
+    --             },
+    --             on_disconnect = {
+    --               clean_mount_folders = false, -- remove mount point folder on disconnect/unmount
+    --             },
+    --             on_edit = {}, -- not yet implemented
+    --           },
+    --           ui = {
+    --             select_prompts = false, -- not yet implemented
+    --             confirm = {
+    --               connect = true, -- prompt y/n when host is selected to connect to
+    --               change_dir = false, -- prompt y/n to change working directory on connection (only applicable if handlers.on_connect.change_dir is enabled)
+    --             },
+    --           },
+    --           log = {
+    --             enable = false, -- enable logging
+    --             truncate = false, -- truncate logs
+    --             types = { -- enabled log types
+    --               all = false,
+    --               util = false,
+    --               handler = false,
+    --               sshfs = false,
+    --             },
+    --           },
+    --         }
+    --     end
+    -- },
     {
         "folke/noice.nvim",
         event = "VeryLazy",
@@ -915,18 +915,18 @@ require("lazy").setup({
             })
         end
     },
-    {
-        "jackMort/ChatGPT.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("chatgpt").setup()
-        end,
-        dependencies = {
-          "MunifTanjim/nui.nvim",
-          "nvim-lua/plenary.nvim",
-          "nvim-telescope/telescope.nvim"
-        }
-    },
+    -- {
+    --     "jackMort/ChatGPT.nvim",
+    --     event = "VeryLazy",
+    --     config = function()
+    --         require("chatgpt").setup()
+    --     end,
+    --     dependencies = {
+    --       "MunifTanjim/nui.nvim",
+    --       "nvim-lua/plenary.nvim",
+    --       "nvim-telescope/telescope.nvim"
+    --     }
+    -- },
     "nvim-lua/popup.nvim",
     {
         "nvim-telescope/telescope.nvim",
