86fb36c8ef5cb48e195c64a3654b98e23f6c02c9
8ucchiman <8ucchiman@gmail.com>
2024-10-05 21:18:32 +0900

    ts lsp fixed

--- nvim/lua/plugins/config/lspconfig.lua
+++ nvim/lua/plugins/config/lspconfig.lua
@@ -44,7 +44,7 @@ require("mason-lspconfig").setup_handlers {
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
-lspconfig.tsserver.setup {}
+-- lspconfig.tsserver.setup {}
 lspconfig.rust_analyzer.setup {
   -- Server-specific settings. See `:help lspconfig-setup`
   settings = {
